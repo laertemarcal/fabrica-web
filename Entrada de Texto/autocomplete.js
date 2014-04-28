98f62823a25beeb0168918bc3518035e1d88dd6f
Laerte Marçal <laerte.marcal@hotmail.com>
2014-04-27 23:11:54 -0300

    Adicionado limite de caractéres e linhas
    Máximo de linhas: 5
    Limite de caracteres: 40
    *Modificado a estilização do JQuery UI Autocomplete

--- Entrada de Texto/autocomplete.js
+++ Entrada de Texto/autocomplete.js
@@ -1,6 +1,8 @@
-$(function () {
-    var times = ['Vasco', 'São Paulo', 'Corinthians', 'Vitória', 'Volta Redonda', 'Fluminense', 'Flamengo', 'Botafogo', 'Sport', 'Santa Cruz', 'Palmeiras', 'Santos', 'Grêmio', 'Internacional', 'Goiás', 'Vila Nova', 'Aparecidense', 'Anápolis', 'Anapolina', 'Atlético GO', 'Atlético MG', 'Real Madrid', 'Juventus', 'Barcelona', 'Manchester United', 'Manchester City', 'Liverpool', 'Chelsea', 'Paris Saint Germain', 'Borussia Dortmund', 'Bayern de Munique'];
+$(function() {
+    var times = ['Vasco da Gama', 'São Paulo', 'Corinthians', 'Vitória', 'Volta Redonda', 'Fluminense', 'Flamengo', 'Botafogo', 'Sport', 'Santa Cruz', 'Palmeiras', 'Santos', 'Grêmio', 'Internacional', 'Goiás', 'Vila Nova', 'Aparecidense', 'Anápolis', 'Anapolina', 'Atlético GO', 'Atlético MG', 'Real Madrid', 'Juventus', 'Barcelona', 'Manchester United', 'Manchester City', 'Liverpool', 'Chelsea', 'Paris Saint Germain', 'Borussia Dortmund', 'Bayern de Munique'];
     var autocomplete = false;
+    var maxChars = 40;
+    var maxLines = 5;
 
     function split(val) {
         return val.split(/ \s*/);
@@ -9,18 +11,19 @@ $(function () {
     function extractLast(term) {
         return split(term).pop();
     }
+
     $("#list").autocomplete({
         //source: manufacturers,
-        source: function (request, response) {
+        source: function(request, response) {
             // delegate back to autocomplete, but extract the last term
             response($.ui.autocomplete.filter(
-            times, extractLast(request.term)));
+                    times, extractLast(request.term)));
         },
-        focus: function () {
+        focus: function() {
             // prevent value inserted on focus
             return false;
         },
-        select: function (event, ui) {
+        select: function(event, ui) {
             //var terms = split(this.value);
             var terms = split(this.value);
             // remove the current input
@@ -33,8 +36,8 @@ $(function () {
             return false;
         }
     })
-        .autocomplete('disable')
-        .bind('keydown', function (event) {
+            .autocomplete('disable')
+            .bind('keydown', function(event) {
         if (event.keyCode === $.ui.keyCode.TAB && $(this).data("ui-autocomplete").menu.active) {
             event.preventDefault();
         }
@@ -46,18 +49,32 @@ $(function () {
         }
         //Detecta se APENAS espaço foi pressionado e desabilita o autocomplete.
         if (!event.ctrlKey && event.keyCode === 32) {
-            $(this).autocomplete('disable');
             if (autocomplete) {
                 autocomplete = false;
                 return false;
             }
+            $(this).autocomplete('close');
+            $(this).autocomplete('disable');
         }
+    })
+            .on('input focus keydown keyup', function(e) {
+        var lineLength = $(this).val().split("\n").length;
+        var text = $(this).val();
+        var lines = text.split(/(\r\n|\n|\r)/gm);
+        if (e.keyCode === 13 && lineLength >= maxLines) {
+            return false;
+        }
+        for (var i = 0; i < lines.length; i++) {
+            if (lines[i].length > maxChars) {
+                lines[i] = lines[i].substring(0, maxChars);
+            }
+        }
+        $(this).val(lines.join(''));
     });
-
     // Sobrescreve o filtro do (JQuery.UI.Autocomplete) filtrando apenas o começo da string. (prefixo)
-    $.ui.autocomplete.filter = function (array, term) {
+    $.ui.autocomplete.filter = function(array, term) {
         var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
-        return $.grep(array, function (value) {
+        return $.grep(array, function(value) {
             return matcher.test(value.label || value.value || value);
         });
     };
